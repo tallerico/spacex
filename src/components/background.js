661670bdb00334750d84deaf805948fd3f5eddcc
Mike Tallerico <mtallerico1@gmail.com>
2019-04-15 19:11:26 -0400

    initial commit

---
+++ src/components/background.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+function MainHeader() {
+	const Header = styled.div`
+		height: 100vh;
+		overflow: hidden;
+	`
+
+	return <Header />
+}
+
+export default MainHeader
