c1b46ec6dbe393b14bd2e4acb1709e013025cf80
Mike Tallerico <mtallerico1@gmail.com>
2019-04-15 20:22:56 -0400

    adds media query for background image

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import headMobile from '../img/Mobie_Circle_Header_1440X2960.svg'
+import headMain from '../img/Circle_Header_1600X1000.svg'
 
 function MainHeader() {
 	const Header = styled.div`
 		height: 100vh;
 		background-image: url(${headMobile});
 		background-repeat: no-repeat;
+		background-size: cover;
+
+		@media (min-width: 940px) {
+			background-image: url(${headMain});
+		}
 	`
 
 	return <Header />
