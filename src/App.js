59c8807db53afd9c8306ff82edc209858b47dfe5
Mike Tallerico <mtallerico1@gmail.com>
2019-04-16 21:25:18 -0400

    imports navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import MainHeader from './components/MainHeader'
+import Navigation from './components/Navigation'
 import './App.css'
 
 class App extends Component {
 	render() {
-		return <MainHeader />
+		return (
+			<div>
+				<Navigation />
+				<MainHeader />
+			</div>
+		)
 	}
 }
 
