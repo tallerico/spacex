551b23cfe66114938bdaaf3dac9663986f15bc00
Mike Tallerico <mtallerico1@gmail.com>
2019-05-23 23:53:43 -0400

    built working timer

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -11,21 +11,28 @@ const countdown = props => {
 	const [minutes, setMinutes] = useState();
 	const [seconds, setSeconds] = useState();
 
+	//gets difference between 2 dates using moment
+	function getDiff(currDate, targetDate) {
+		const diff = targetDate.diff(currDate);
+		const diffDuration = moment.duration(diff);
+		//setting state with data for next launch
+		setDays(diffDuration.days());
+		setHours(diffDuration.hours());
+		setMinutes(diffDuration.minutes());
+		setSeconds(diffDuration.seconds());
+	}
+
 	useEffect(() => {
 		axios.get('https://api.spacexdata.com/v3/launches/next').then(function(response) {
 			//getting current date and time
 			const currentDate = moment().format();
 			//setting launch time to the moment object
 			const launchDate = moment.utc(response.data.launch_date_local);
-			//getting difference between dates
-			const diff = launchDate.diff(currentDate);
-			//express as a duration
-			const diffDuration = moment.duration(diff);
-			//setting state with data for next launch
-			setDays(diffDuration.days());
-			setHours(diffDuration.hours());
-			setMinutes(diffDuration.minutes());
-			setSeconds(diffDuration.seconds());
+			getDiff(currentDate, launchDate);
+			setTimeout(() => {
+				const newCurrDate = moment().format();
+				getDiff(newCurrDate, launchDate);
+			}, 1000);
 		});
 	});
 
