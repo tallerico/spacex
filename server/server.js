a129fa5076b7da9940b0fd4acf0f6c480d08a6fc
Mike Tallerico <mtallerico1@gmail.com>
2019-05-25 16:28:33 -0400

    initial commit for express server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/timetolaunch', function(req, res, next) {
+	axios
+		.get('https://api.spacexdata.com/v3/launches/next')
+		.catch(next)
+		.then(response => {
+			console.log(response);
+			res.send(response.data);
+		});
+});
