e0515b449763ebc5706b923304bbe74b8c678b14
Mike Tallerico <mtallerico1@gmail.com>
2019-05-26 15:12:58 -0400

    adjust background color

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
 	root: {
 		flexGrow: 1,
-		background: '#666666',
+		background: '#605c5e',
 	},
 	grow: {
 		flexGrow: 1,
@@ -21,10 +21,10 @@ const styles = {
 		marginLeft: -12,
 		marginRight: 20,
 	},
-}
+};
 
 function ButtonAppBar(props) {
-	const { classes } = props
+	const { classes } = props;
 	return (
 		<AppBar classes={{ root: classes.root }} position="static">
 			<Toolbar>
@@ -37,11 +37,11 @@ function ButtonAppBar(props) {
 				<Button color="inherit">Login</Button>
 			</Toolbar>
 		</AppBar>
-	)
+	);
 }
 
 ButtonAppBar.propTypes = {
 	classes: PropTypes.object.isRequired,
-}
+};
 
-export default withStyles(styles)(ButtonAppBar)
+export default withStyles(styles)(ButtonAppBar);
