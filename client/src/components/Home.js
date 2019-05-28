5d3ccb0dadcc5011ddbf725e00707be8790282a9
Mike Tallerico <mtallerico1@gmail.com>
2019-05-27 23:44:29 -0400

    imports next flight component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Countdown from './countdown';
+import NextLaunch from './NextLaunch';
 
 function home() {
-	return <Countdown />;
+	return <NextLaunch />;
 }
 
 export default home;
