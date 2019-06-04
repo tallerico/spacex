68c49c309fe7e7d5ad20881ce7383fabfa86cd4e
Mike Tallerico <mtallerico1@gmail.com>
2019-06-04 10:15:59 -0400

    fetching data from higher level component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NextLaunch from './NextLaunch';
+import Card from './DataBlock';
+import Section from './Section';
+import axios from 'axios';
 
 function home() {
-	return <NextLaunch />;
+	const [launchData, setLaunchData] = useState();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios.get('/api/timetolaunch');
+			setLaunchData(result.data);
+			console.log(result);
+		};
+
+		fetchData();
+	}, []);
+	return (
+		<div>
+			<NextLaunch launch={launchData} />
+			<Section>
+				<Card>
+					<p>Hello</p>
+				</Card>
+			</Section>
+		</div>
+	);
 }
 
 export default home;
